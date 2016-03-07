6cbe3665cb204d7871b93c81b593ffc88c650f0f
Enrique Cahua <enriquecahua@outlook.com>
2016-03-06 21:02:42 -0800

    Added a gruntfile and added files to the /src directory git push origin master

---
+++ src/js/resumeBuilder.js
@@ -0,0 +1,425 @@
+var bio = {
+    "name": "Enrique Cahua ",
+    "role": "Software Developer",
+    "contacts": {
+        "email": "enriquecahua.com",
+        "github": "enriquecahua",
+        "twitter": "@enriquecahua",
+        "location": "Riverside, California"
+    },
+    "picture": "me.jpg",
+    "welcomeMessage": "I'm currently looking for a position as a web developer.  Thanks for stopping by!",
+    "skills": ["Java", "python", "Software Development", "Test Driven Development", "Retrospective Facilitation"]
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Amazon",
+            "title": "Data Analyst",
+            "location": "Moreno Valley, California",
+            "datesWorked": "Septmeber 2014 - January 2016",
+            "description": "The position responsibilities included designing queries, compiling data, and  generating reports. This required research into defect trends, assisting management with warehouse capacity requirements and maintaining the most efficient process paths. This work includes, but is not limited to, database queries, Excel dashboards, and VBA programming. "
+		},
+        {
+            "employer": "L.A. Fitness",
+            "title": "Salesperson",
+            "location": "Riverside, California",
+            "datesWorked": "July 2014 -September 2014",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "Sears",
+			"title": "Salesperson",
+			"location": "Moreno Valley, California",
+			"datesWorked": "June 2013 - July 2014",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "14 St. bar",
+			"title": "Bartender",
+			"location": "New York, New York",
+			"datesWorked": "March 2012 - November 2012",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "Cater One",
+			"title": "Bartender",
+			"location": "New york, New York",
+			"datesWorked": "January 2011 - November 2012",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "FontaineBleau Resort",
+			"title": "Bartender",
+			"location": "Miami Beach, Florida",
+			"datesWorked": "February 2009 - November 2010",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "Cabana One ",
+			"title": "Bartender",
+			"location": "Coral Gables, Florida",
+			"datesWorked": "November 2008 - March 2009",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		},
+		{
+			"employer": "Nocturnal",
+			"title": "Bartender",
+			"location": "Miami, Florida",
+			"datesWorked": "Decemeber 2006 - December 2008",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue metus, lacinia sed ornare a, varius eu metus. Sed condimentum sapien et arcu gravida mattis. Morbi vehicula vestibulum ligula. Vestibulum dolor est, pulvinar et ullamcorper in, sagittis vel justo. Cras id malesuada magna. Quisque rhoncus magna enim, sit amet tincidunt mauris pellentesque ultrices. Vivamus facilisis pharetra dignissim."
+		}
+	]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "University of Illinois, Springfield",
+            "datesAttended": "2013 - current",
+            "location": "Riverside, California",
+            "degree": "B.S. Software Engineering",
+            "major": "Computer Science",
+            "url": "www.uis.edu"
+		},
+        {
+            "name": "Rutgers University",
+            "datesAttended": "2011-2012",
+            "location": "Newark, New Jersey",
+            "degree": "N/A",
+            "major": "Computer Science",
+            "url": "www.rutgers.edu"
+        },
+        {
+            "name": "Miami Dade College",
+            "datesAttended": "2007-2010",
+            "location": "Miami, Florida",
+            "degree": "A.S. Finance",
+            "major": "Finance",
+            "url": "www.mdc.edu"
+        },
+        {
+            "name": "Burlingame High School",
+            "datesAttended": "2002-2006",
+            "location": "Burlingame, California",
+            "degree": "Diploma",
+            "major": "General Education",
+            "url": "www.bhs.edu"
+        }
+	],
+    "onlineCourses": [
+        {
+            "school": "Udacity",
+            "title": "Object-Oriented Javascript",
+            "completed": "November 2014",
+            "url": "https://www.udacity.com/course/ud015"
+		},
+        {
+            "school": "Udacity",
+            "title": "HTML5 Canvas",
+            "completed": "November 2014",
+            "url": "https://www.udacity.com/course/ud292"
+		},
+        {
+            "school": "Udacity",
+            "title": "Javascript Basics",
+            "completed": "October 2014",
+            "url": "https://www.udacity.com/course/ud804"
+		},
+        {
+            "school": "Udacity",
+            "title": "Intro to HTML and CSS",
+            "completed": "October 2014",
+            "url": "https://www.udacity.com/course/ud304"
+		},
+        {
+            "school": "Udacity",
+            "title": "Web Development",
+            "completed": "August 2014",
+            "url": "https://www.udacity.com/course/cs253"
+		}
+	]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "HTML5 Canvas Game",
+            "datesWorked": "December 2014 - January 2015",
+            "description": "Created an online game using HTML5 Canvas as part of Udacity's Front-End Web Developer " +
+                "Nanodegree.",
+            "images": ["http://lorempixel.com/150/150/"],
+            "url": "#"
+		},
+        {
+            "title": "Online Portfolio",
+            "datesWorked": "October 2014",
+            "description": "Created an online portfolio of work as part of Udacity's Front-End Web Developer " +
+                "Nanodegree.",
+            "images": ["http://lorempixel.com/150/151/"],
+            "url": "#"
+		},
+        {
+            "title": "Web Development Wiki",
+            "datesWorked": "June 2014 - August 2014",
+            "description": "Created an online wiki for Udacity's Web Development Course.",
+            "images": ["http://lorempixel.com/151/150/"],
+            "url": "#"
+		}
+	]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+
+    for (i in bio.skills) {
+        $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+    }
+}
+
+for (i in formattedContactInfo) {
+    $("#topContacts").append(formattedContactInfo[i]);
+    $("#footerContacts").append(formattedContactInfo[i]);
+
+}
+
+// display function from Lesson 2
+function displayWork() {
+
+    if (work.jobs.length > 0) {
+
+        $("#workExperience").append(HTMLworkStart);
+
+        for (job in work.jobs) {
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+            var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[job].datesWorked);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+            var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+            $(".work-entry:last").append(formattedEmployerWorkTitle);
+            $(".work-entry:last").append(formattedWorkLocation);
+            $(".work-entry:last").append(formattedDatesWorked);
+            $(".work-entry:last").append(formattedWorkDescription);
+        }
+
+    }
+
+}
+
+displayWork();
+
+
+projects.display = function () {
+    if (projects.projects.length > 0) {
+        for (i in projects.projects) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+            $(".project-entry:last").append(formattedProjectTitle);
+            $(".project-entry:last").append(formattedProjectDates);
+            $(".project-entry:last").append(formattedProjectDescription);
+
+            for (img in projects.projects[i].images) {
+                var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+                $(".project-entry:last").append(formattedProjectImage);
+            }
+
+
+        }
+    }
+}
+
+projects.display();
+
+education.display = function () {
+    if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+        for (i in education.schools) {
+            $("#education").append(HTMLschoolStart);
+
+            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+            var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor);
+
+            $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+            $(".education-entry:last").append(formattedSchoolDates);
+            $(".education-entry:last").append(formattedSchoolLocation);
+            $(".education-entry:last").append(formattedSchoolMajor);
+            $(".education-entry:last").append(formattedSchoolMinor);
+        }
+
+        if (education.onlineCourses.length > 0) {
+            $("#education").append(HTMLonlineClasses);
+            for (i in education.onlineCourses) {
+                $("#education").append(HTMLschoolStart);
+                var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+                var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+                var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+                var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+                $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+                $(".education-entry:last").append(formattedOnlineDates);
+                $(".education-entry:last").append(formattedOnlineURL);
+            }
+        }
+
+    }
+}
+
+education.display();
+
+
+$("#main").append(internationalizeButton);
+
+function inName(name) {
+
+    var myArr = name.split(" ");
+
+    //var firstLetter = nameArr[0].charAt(0).toUpperCase();
+    //var firstName = nameArr[0].substr(1).toLowerCase();
+
+
+
+    return myArr[0].charAt(0).toUpperCase() + myArr[0].substr(1).toLowerCase() + " " + myArr[1].toUpperCase();
+
+}
+
+// Skills Chart at 750px
+
+$(function () {
+    $('#graphcontainer').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: '',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 40
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Strength',
+            innerSize: '50%',
+            data: [
+                ['HTML5', 10.00],
+                ['CSS3', 15.00],
+                ['JavaScript', 50.00],
+     			['SQL', 15.00],
+                ['Java', 10.00],
+                {
+                    name: 'Proprietary or Undetectable',
+                    y: 0.2,
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            ]
+        }]
+    });
+});
+
+$(function () {
+    $('#graphcontainer').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: { 
+            text: '',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 40
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Strength',
+            innerSize: '50%',
+            data: [
+                ['HTML5',   10.00],
+                ['CSS3', 15.00],
+                ['JavaScript',  50.00],
+     						['SQL',     15.00],
+                ['Java',  10.00],
+                {
+                    name: 'Proprietary or Undetectable',
+                    y: 0.2,
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            ]
+        }]
+    });
+});
+
+
+$("#mapDiv").append(googleMap);
